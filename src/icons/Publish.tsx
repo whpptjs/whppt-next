0b276a08f9d1630eec4431e8d7ece66fe671be86
Ben_G <ben@sveltestudios.com>
2022-07-05 16:07:45 +0930

    domains saving and editing

--- src/icons/Publish.tsx
+++ src/icons/Publish.tsx
@@ -4,10 +4,17 @@ export const WhpptIconPublish: FC = ({}) => {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
-      viewBox="0 0 20 20"
-      className="fill-current stroke-current"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth="2"
     >
-      <path d="M0 0l20 10L0 20V0zm0 8v4l10-2L0 8z" fill="currentColor" />
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z"
+      />
     </svg>
   );
 };
