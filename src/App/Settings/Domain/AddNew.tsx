81527d332dbe5dd381fa1a920f439de74fc78a6d
Ben_G <ben@sveltestudios.com>
2022-07-06 11:37:47 +0930

    config done

--- src/App/Settings/Domain/AddNew.tsx
+++ src/App/Settings/Domain/AddNew.tsx
@@ -1,69 +1,89 @@
 import React, { FC, useState } from 'react';
+import { Formik } from 'formik';
+import { Domain } from '../../Model';
 import { useWhppt } from '../../../Context';
 import { WhpptInput, WhpptButton } from '../../../ui/components';
 
 export const DomainAddNewForm: FC = () => {
   const { api } = useWhppt();
-  const [newDomain, setNewDomain] = useState({
+  const [newDomain] = useState({
     name: '',
-    hostNames: [],
+    hostNames: '',
     published: false,
     createdAt: new Date(),
   });
 
-  const addItem = () => {
-    //TODO check fields are done.
-    api.app.domain.save(newDomain).then(() => {
-      setNewDomain({
-        name: '',
-        hostNames: [],
-        published: false,
-        createdAt: new Date(),
-      });
-    });
+  const addItem = (newDomain: Domain) => {
+    return api.app.domain.save(newDomain);
   };
 
   return (
     <section className="whppt-form-section whppt-form-section--bottom-gap">
-      <form>
-        <h4>Add a new domain</h4>
-        <div className="whppt-form-split">
-          <div className="whppt-form-split--even">
-            <WhpptInput
-              value={newDomain.name}
-              onChange={(name: string) => {
-                setNewDomain({ ...newDomain, name });
-              }}
-              id={'newDomainName'}
-              label={'Name'}
-              info={''}
-              error={''}
-              type={'text'}
-            />
-          </div>
-          <div className="whppt-form-split--even">
-            <WhpptInput
-              value={newDomain.hostNames.join(',')}
-              onChange={(hostNames: string) => {
-                setNewDomain({
-                  ...newDomain,
-                  hostNames: hostNames.split(',').map((h) => h.trim()),
-                });
-              }}
-              id={'newDomainhostnames'}
-              label={'Host names'}
-              info={
-                'Comma seperate items without the www eg: whppt.com,sveltestudios.com'
-              }
-              error={''}
-              type={'text'}
-            />
-          </div>
-        </div>
-        <div>
-          <WhpptButton text="Add New Domain" onClick={addItem} />
-        </div>
-      </form>
+      <h1>Sign Up</h1>
+      <Formik
+        initialValues={newDomain}
+        validate={(values) => {
+          const errors = {} as any;
+          if (!values.name) {
+            errors.name = 'Required';
+          }
+          if (!values.hostNames) {
+            errors.hostNames = 'Required';
+          }
+          return errors;
+        }}
+        onSubmit={(values, { resetForm }) => {
+          addItem({
+            ...values,
+            hostNames: values.hostNames.split(',').map((h) => h.trim()),
+          }).then(() => {
+            resetForm();
+          });
+        }}
+      >
+        {(props) => (
+          <form onSubmit={props.handleSubmit}>
+            <h4>Add a new domain</h4>
+            <div className="whppt-form-split">
+              <div className="whppt-form-split--even">
+                <WhpptInput
+                  value={props.values.name}
+                  onChangeEvent={props.handleChange}
+                  onChange={() => {}}
+                  id={'newDomainName'}
+                  label={'Name'}
+                  info={''}
+                  error={props.errors.name}
+                  type={'text'}
+                  name="name"
+                />
+              </div>
+              <div className="whppt-form-split--even">
+                <WhpptInput
+                  value={props.values.hostNames}
+                  onChangeEvent={props.handleChange}
+                  onChange={() => {}}
+                  id={'newDomainhostnames'}
+                  label={'Host names'}
+                  info={
+                    'Comma seperate items without the www eg: whppt.com,sveltestudios.com'
+                  }
+                  error={props.errors.hostNames}
+                  type={'text'}
+                  name="hostNames"
+                />
+              </div>
+            </div>
+            <div>
+              <WhpptButton
+                text="Add New Domain"
+                type="submit"
+                onClick={() => props.handleSubmit}
+              />
+            </div>
+          </form>
+        )}
+      </Formik>
     </section>
   );
 };
