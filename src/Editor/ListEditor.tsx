a9a73a3f797acdc272088293713008992930d5b3
Terence <terence@sveltestudios.com>
2022-07-07 10:33:49 +0930

    Editors options moved to props

--- src/Editor/ListEditor.tsx
+++ src/Editor/ListEditor.tsx
@@ -1,18 +1,19 @@
-import React, { FC, ReactElement } from 'react';
-import { ListEditorOptions } from '../editors/List';
-import { useWhppt } from '../Context';
+import React, { FC, ReactElement } from "react";
+import { ListEditorOptions } from "../editors/List";
+import { useWhppt } from "../Context";
 
 export const ListEditor: FC<{
   value: any;
   onChange: (value: any) => void;
-  options: ListEditorOptions;
+  addNew: <T>() => T;
   children: ({ isEditing }: { isEditing: boolean }) => ReactElement;
-}> = ({ children, value, options, onChange }) => {
+}> = ({ children, value, addNew, onChange }) => {
   const { editing, showEditor } = useWhppt();
+  const options = { addNew } as ListEditorOptions;
   return (
     <div
       className="whppt-editor-selector"
-      onClick={() => showEditor('list', value, onChange, options)}
+      onClick={() => showEditor("list", value, onChange, options)}
     >
       {children({ isEditing: editing })}
     </div>
--- src/Editor/PlainTextEditor.tsx
+++ src/Editor/PlainTextEditor.tsx
@@ -1,18 +1,19 @@
-import React, { FC, ReactElement } from 'react';
-import { BasicEditorOptions } from '../editors/PlainText';
-import { useWhppt } from '../Context';
+import React, { FC, ReactElement } from "react";
+import { BasicEditorOptions } from "../editors/PlainText";
+import { useWhppt } from "../Context";
 
 export const PlainTextEditor: FC<{
   value: any;
   onChange: (value: any) => void;
-  options: BasicEditorOptions;
+  label: string;
   children: ({ isEditing }: { isEditing: boolean }) => ReactElement;
-}> = ({ children, value, options, onChange }) => {
+}> = ({ children, value, label, onChange }) => {
   const { editing, showEditor } = useWhppt();
+  const options = { label } as BasicEditorOptions;
   return (
     <div
       className="whppt-editor-selector"
-      onClick={() => showEditor('plainText', value, onChange, options)}
+      onClick={() => showEditor("plainText", value, onChange, options)}
     >
       {children({ isEditing: editing })}
     </div>
