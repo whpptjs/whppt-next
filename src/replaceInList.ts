5a665e766e99d57d4fb32b948f11734e618b9e23
Terence <terence@sveltestudios.com>
2022-07-15 12:39:51 +0930

    Replace in list function to stringify the ids for error messages

--- src/replaceInList.ts
+++ src/replaceInList.ts
@@ -1,10 +1,8 @@
 export const replaceInList = function <T extends object>(items: T[], item: T, id: keyof T = '_id' as keyof T): T[] {
-  console.log('🚀 ~ file: index.tsx ~ line 23 ~ item', item);
-  console.log('🚀 ~ file: index.tsx ~ line 23 ~ items', items);
-  if (!Object.keys(item).find(k => k === id)) throw new Error(`New item missing property ${id}`);
+  if (!Object.keys(item).find(k => k === id)) throw new Error(`New item missing property ${id.toString()}`);
 
   const oldItem = items.find(i => i[id] === item[id]);
-  if (!oldItem) throw new Error(`Original item with ${id} = ${item[id]} not found`);
+  if (!oldItem) throw new Error(`Original item with ${id.toString()} = ${item[id].toString()} not found`);
 
   const oldItemIndex = items.indexOf(oldItem);
   const newItems = [...items];
