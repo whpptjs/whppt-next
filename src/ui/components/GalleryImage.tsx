d902f512ffa44257e7abdbc9de2b2235188e10a8
leandrosveltestudios <leandro@sveltestudios.com>
2022-07-18 17:23:47 +1000

    crud from gallery

---
+++ src/ui/components/GalleryImage.tsx
@@ -0,0 +1,38 @@
+import React, { FC, useState } from 'react';
+
+type WhpptGalleryImageProps = {
+  url: string;
+  remove: () => void;
+  name: string;
+};
+
+export const WhpptGalleryImage: FC<WhpptGalleryImageProps> = ({ url, remove, name }) => {
+  const [showDelete, setShowDelete] = useState(false);
+  const [loaded, setLoaded] = useState(false);
+
+  return (
+    <div style={{ position: 'relative' }}>
+      {loaded ? null : (
+        <div style={{ height: 200, width: 360 }}>
+          <p>Loading ...</p>
+        </div>
+      )}
+
+      <div style={loaded ? {} : { display: 'none' }}>
+        <img
+          style={{ height: 200, width: 360, objectFit: 'cover', cursor: 'pointer' }}
+          onLoad={() => setLoaded(true)}
+          src={url}
+          onMouseEnter={() => setShowDelete(true)}
+          onMouseLeave={() => setShowDelete(false)}
+        />
+        {showDelete && (
+          <div onClick={remove} className="whppt-gallery-delete" onMouseEnter={() => setShowDelete(true)}>
+            Delete
+          </div>
+        )}
+        <p>{name}</p>
+      </div>
+    </div>
+  );
+};
