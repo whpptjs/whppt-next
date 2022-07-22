ff82256b466f74294a81582138c4f321cdbfd230
Ben Gill <ben@Bens-MacBook-Pro.local>
2022-07-22 13:27:06 +0930

    merging

--- src/Editor/ContentsTree/Panel.tsx
+++ src/Editor/ContentsTree/Panel.tsx
@@ -1,4 +1,6 @@
 import React, { FC, useEffect, useMemo } from 'react';
+import { SortableContainer, SortableElement } from 'react-sortable-hoc';
+import { ComponentData } from '../../..';
 import { useWhppt } from '../../Context';
 
 export const WhpptContentsTreeEditor: FC = () => {
@@ -10,5 +12,51 @@ export const WhpptContentsTreeEditor: FC = () => {
   useEffect(() => {
     console.log('🚀 ~ file: Panel.tsx ~ line 13 ~ tree', tree);
   }, [tree]);
-  return <div>test</div>;
+
+  const SortableItem = SortableElement<{ item: ComponentData }>(({ item, index }) => (
+    <div className="whppt-content-tree-list__inner-list" key={`whppt-contents-child-${index}`}>
+      {item.definitionKey}
+    </div>
+  ));
+
+  const onSortEnd = () => {
+    // const onSortEnd = ({ oldIndex, newIndex }) => {
+    // onChange(arrayMoveImmutable(value, oldIndex, newIndex));
+    // var el = document.querySelector('li.whppt-sortable-list__item.sortableHelper');
+    // el && el.parentNode.removeChild(el);
+  };
+
+  const SortableList = SortableContainer<{ items: ComponentData[][] }>(({ items }) => {
+    return (
+      <ul>
+        {items.map((list, key) => (
+          <div className="whppt-content-tree-list" key={`whppt-contents-${key}`}>
+            {list.map((content, index) => {
+              return <SortableItem key={`whppt-contents-${key}`} index={index} item={content} />;
+            })}
+          </div>
+        ))}
+      </ul>
+    );
+  });
+
+  return (
+    <div>
+      <SortableList distance={10} helperClass="sortableHelper" items={tree} onSortEnd={onSortEnd} />
+
+      {/* {tree.map((contents, key) => {
+        return (
+          <div className="whppt-content-tree-list" key={`whppt-contents-${key}`}>
+            {contents.map((content, index) => {
+              return (
+                <div className="whppt-content-tree-list__inner-list" key={`whppt-contents-child-${index}`}>
+                  {content.definitionKey}
+                </div>
+              );
+            })}
+          </div>
+        );
+      })} */}
+    </div>
+  );
 };
