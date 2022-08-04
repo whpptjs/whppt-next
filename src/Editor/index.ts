1e538ff66cbfa9e30f1193488be0ab886696fb9b
Terence <terence@sveltestudios.com>
2022-08-04 13:09:23 +0930

    export editor args type

--- src/Editor/index.ts
+++ src/Editor/index.ts
@@ -2,4 +2,5 @@ export * from './EditorPanel';
 export * from './Editors';
 export * from './Panels';
 export * from './EditorArgs';
+export * from './EditorOptions';
 export * from './UseDefaultEditors';
