46694750a35fa068a0e75ab4e645e6a2d15cff0c
Terence <terence@sveltestudios.com>
2022-08-05 11:01:31 +0930

    working with objects and taking the needed fields with default

--- src/ui/components/Select.tsx
+++ src/ui/components/Select.tsx
@@ -1,20 +1,24 @@
 import React from 'react';
 import Select from 'react-select';
 
-type WhpptSelectProps<T, R> = {
-  items: T[];
+type WhpptSelectProps<T> = {
   id?: string;
   name?: string;
+  idField?: keyof T;
   label: string;
+  items: T[];
+  value?: T;
+  onChange: (item: T) => void;
+  getOptionLabel: (option: T) => string;
+  isOptionSelected?: (option: T) => boolean;
   isDisabled?: boolean;
   isLoading?: boolean;
   isClearable?: boolean;
   isSearchable?: boolean;
-  onChange: (i: T) => void;
-  value?: R;
+  error?: string;
 };
 
-export const WhpptSelect = <T, R>({
+export const WhpptSelect = <T extends {}>({
   id,
   name,
   label,
@@ -22,10 +26,17 @@ export const WhpptSelect = <T, R>({
   isDisabled = false,
   isLoading = false,
   isClearable = false,
-  isSearchable = false,
+  isSearchable = true,
   onChange,
-}: // value,
-WhpptSelectProps<T, R>) => {
+  error,
+  value,
+  getOptionLabel,
+  isOptionSelected,
+  idField = '_id' as keyof T,
+  ...props
+}: WhpptSelectProps<T>) => {
+  const _isOptionSelected = (item: T) => item && value && item[idField] === value[idField];
+
   return (
     <div>
       <label htmlFor={id}>{label}</label>
@@ -41,8 +52,22 @@ WhpptSelectProps<T, R>) => {
         name={name}
         onChange={e => onChange(e)}
         options={items}
-        // value={value}
+        value={value}
+        getOptionLabel={getOptionLabel}
+        isOptionSelected={isOptionSelected || _isOptionSelected}
+        {...props}
       />
+
+      {error && typeof error === 'string' && <span className="whppt-input-error">{error}</span>}
+      {error && Array.isArray(error) && (
+        <div>
+          {error.map((err, index) => (
+            <span key={index} className="whppt-input-error">
+              {err} {index + 1 < error.length && <span>, </span>}
+            </span>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
