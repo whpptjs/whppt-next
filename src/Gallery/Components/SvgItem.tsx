ad4cf839377ce6b8ebe6539d695b1ed3d814ed84
leandrosveltestudios <leandro@sveltestudios.com>
2022-08-11 11:47:25 +1000

    added svg load

---
+++ src/Gallery/Components/SvgItem.tsx
@@ -0,0 +1,38 @@
+import React, { FC, useState, useEffect } from 'react';
+import parse from 'html-react-parser';
+import { useWhppt } from '../../Context';
+
+type WhpptSvgItemProps = {
+  itemId: string | number;
+  name: string;
+};
+
+export const WhpptSvgItem: FC<WhpptSvgItemProps> = ({ itemId, name }) => {
+  const { api } = useWhppt();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [svgString, setSvgString] = useState('');
+
+  useEffect(() => {
+    //TODO: Check if we will return Svg or gallery Item
+    api.gallery.loadSvg
+      .load('_id')
+      .then(res => res.text())
+      .then(svgString => {
+        setSvgString(svgString);
+        setLoading(true);
+      })
+      .catch(error => setError(error.message || error));
+  }, [itemId]);
+
+  return error ? (
+    <p>Svg could not be loaded</p>
+  ) : loading ? (
+    <p>loading ...</p>
+  ) : (
+    <div className="whppt-gallery-grid--svgs svg-container">
+      {parse(svgString)}
+      {<p className="whppt-gallery-grid--svgs svg-title">{name}</p>}
+    </div>
+  );
+};
