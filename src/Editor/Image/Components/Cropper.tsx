dbd2287ec348c76864c8351ed19039c9ddb1cb9b
leandrosveltestudios <leandro@sveltestudios.com>
2022-08-22 15:52:12 +1000

    removed default coordinates calculation and let library handle

--- src/Editor/Image/Components/Cropper.tsx
+++ src/Editor/Image/Components/Cropper.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo, useCallback, useState } from 'react';
+import React, { FC, useMemo } from 'react';
 import { CropperRef, Cropper } from 'react-advanced-cropper';
 import { WhpptImageCrop } from '../Model/ImageData';
 
@@ -19,7 +19,6 @@ export const getPortraitRatio = ratio => {
 
 export const WhpptCropper: FC<WhpptCropperProps> = ({ value, onChange }) => {
   const getImgUrl = galleryItemId => `${process.env.NEXT_PUBLIC_BASE_API_URL}/gallery/image/${galleryItemId}`;
-  const [imageSize, setImageSize] = useState(null);
 
   const stencilAspectRatio = useMemo(() => {
     const orientation = value.orientation;
@@ -27,52 +26,19 @@ export const WhpptCropper: FC<WhpptCropperProps> = ({ value, onChange }) => {
     return orientation === 'landscape' ? getLandscapeRatio(aspectRatio?.ratio) : getPortraitRatio(aspectRatio?.ratio);
   }, [value]);
 
-  const cropperRef = useCallback(
-    cropper => {
-      if (!imageSize) {
-        const image = cropper?.getImage();
-
-        image &&
-          setImageSize({
-            height: image.height * (isNaN(stencilAspectRatio) ? 1 : stencilAspectRatio),
-            width: image.width,
-            top: 0,
-            left: 0,
-          });
-      }
-    },
-    [imageSize, stencilAspectRatio]
-  );
-
-  const valueCoords = useMemo(() => {
-    return (
-      value.coords ||
-      (imageSize && {
-        height: imageSize.height * (isNaN(stencilAspectRatio) ? 1 : stencilAspectRatio),
-        width: imageSize.width,
-        top: 0,
-        left: 0,
-      })
-    );
-  }, [stencilAspectRatio, value, imageSize]);
-
   const onCrop = (cropper: CropperRef) => {
     const coords = cropper.getCoordinates();
-
-    if (!valueCoords) return;
-
     onChange({ ...value, coords: coords || value.coords });
   };
 
   return (
     <Cropper
-      ref={cropperRef}
       src={getImgUrl(value.galleryItemId)}
       className="whppt-image-editor-panel__cropper"
       onChange={onCrop}
       backgroundClassName={'whppt-cropper-background'}
       stencilProps={{ aspectRatio: stencilAspectRatio, lines: true }}
-      defaultCoordinates={valueCoords}
+      defaultCoordinates={value?.coords || null}
     />
   );
 };
