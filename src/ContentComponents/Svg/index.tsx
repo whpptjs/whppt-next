53ee09c7aec21057e31bbde47cd3b78f6628b07d
Edward Vercoe <edward@sveltestudios.com>
2022-09-06 10:44:51 +1000

    svg editor changes

--- src/ContentComponents/Svg/index.tsx
+++ src/ContentComponents/Svg/index.tsx
@@ -1,53 +1,21 @@
-import React, { FC, useEffect, useState, useCallback } from 'react';
+import React, { FC } from 'react';
 import { useWhppt } from '../../Context';
 import { ComponentArgs } from '../ComponentData';
 import { SvgEditor } from '../../Editor/Svg/Editor';
-import parse from 'html-react-parser';
 import { WhpptSvgData } from '../../Editor/Svg/SvgData';
+import { WhpptSvg } from '../../ui/components/Svg';
 
 export type SvgComponentData = {
   svg: WhpptSvgData;
 };
 
 export const SvgComponent: FC<ComponentArgs<SvgComponentData>> = ({ data, onChange }) => {
-  const { editing, api } = useWhppt();
-
-  const [created, setCreated] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState('');
-  const [svgString, setSvgString] = useState('');
-
-  const loadSvgString = useCallback(() => {
-    setLoading(true);
-    api.gallery
-      .loadSvg(data?.svg?.galleryItemId)
-      .then(setSvgString)
-      .catch(err => setError(err.message || err))
-      .finally(() => setLoading(false));
-  }, [api.gallery, data?.svg?.galleryItemId]);
-
-  useEffect(() => {
-    if (!created) return setCreated(true);
-
-    setSvgString('');
-
-    if (data?.svg?.galleryItemId) {
-      loadSvgString();
-    }
-  }, [created, data?.svg?.galleryItemId, loadSvgString]);
+  const { editing } = useWhppt();
 
   return (
     <div className={editing ? 'whppt-content--hovered' : ''} onClick={e => e.stopPropagation()}>
       <SvgEditor value={data?.svg} onChange={val => onChange({ ...data, svg: val })}>
-        {error ? (
-          <p>There was an error loading your SVG</p>
-        ) : loading ? (
-          <p>Loading your SVG...</p>
-        ) : svgString ? (
-          <div>{parse(svgString)}</div>
-        ) : (
-          <h1>Select your svg</h1>
-        )}
+        {data.svg && <WhpptSvg data={data.svg} />}
       </SvgEditor>
     </div>
   );
