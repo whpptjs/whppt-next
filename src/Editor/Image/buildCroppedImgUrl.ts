e60f5050a6c44c2ef3692450a0b1bfba86f45d94
Ben_G <ben@sveltestudios.com>
2023-01-17 13:42:23 +1030

    Fixing image url

--- src/Editor/Image/buildCroppedImgUrl.ts
+++ src/Editor/Image/buildCroppedImgUrl.ts
@@ -1,10 +1,15 @@
 import { WhpptImageCrop } from './Model/ImageData';
 import { appendApiKey } from '../../Api/Http';
 
-export const buildCroppedImgUrl = (image: WhpptImageCrop, { height, width, scale }: { height: number; width: number; scale?: number }) => {
-  const _width = scale ? scale * width : width;
-  const _height = scale ? scale * height : height;
-  const baseParams = [`w=${_width}`, `h=${_height}`];
+export const buildCroppedImgUrl = (
+  image: WhpptImageCrop,
+  { height, width, scale }: { height?: number; width?: number; scale?: number }
+) => {
+  const _width = scale && width ? scale * width : width;
+  const _height = scale && height ? scale * height : height;
+  const baseParams = [];
+  if (_width) baseParams.push(`w=${_width}`);
+  if (_height) baseParams.push(`h=${_height}`);
   const coordParams = () => [`cw=${image.coords.width}`, `ch=${image.coords.height}`, `cx=${image.coords.left}`, `cy=${image.coords.top}`];
   const params = [...baseParams, ...(image.coords ? coordParams() : [])];
   const pathWithImageParams = `${process.env.NEXT_PUBLIC_BASE_CDN_API_URL || process.env.NEXT_PUBLIC_BASE_API_URL}/gallery/image/${
